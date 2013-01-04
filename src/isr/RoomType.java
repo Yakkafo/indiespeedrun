a9c96ee3eb7c00a5ed8ebc7917a5443ec1919f55
unknown <emmanuel.corno@neuf.fr>
2013-01-04 22:06:25 +0100

    update room type

--- src/isr/RoomType.java
+++ src/isr/RoomType.java
@@ -1,21 +1,25 @@
 package isr;
 
 public enum RoomType {
-	ENGINE ("Engine room", 3, "char1.jpg"),
-	HOLD ("Hold room", 1, "char2.jpg"),
-	DORM ("Dormitory", 2, "char3.jpg"),
-	CELL ("Cell", 1, "char4.jpg"),
-	COMMON ("Common room", 2, "char5.jpg"),
-	CORRIDOR ("Corridor", 6, "char6.jpg");
+	ENGINE ("Engine room", 3, "engine.png", 10, 10),
+	HOLD ("Hold room", 1, "hold.png", 20, 20),
+	DORM ("Dormitory", 2, "dorm.png", 100, 10),
+	CELL ("Cell", 1, "sousmarin.png", 10, 100),
+	COMMON ("Common room", 2, "commonroom.png", 60, 60),
+	CORRIDOR ("Corridor", 6, "corridor.png", 200, 200),
+	COMMAND ("Command room", 0, "command.png", 550, 550);
 	
 	public String name;
 	public int size;
 	public String spriteName;
+	public int x, y;
 	
-	private RoomType(String name, int size, String spriteName)
+	private RoomType(String name, int size, String spriteName, int x, int y)
 	{
 		this.name = name;
 		this.size = size;
-		this.spriteName = spriteName;	
+		this.spriteName = spriteName;
+		this.x = x;
+		this.y = y;
 	}
 }
