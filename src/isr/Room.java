bc1093c470c03a326f22e87eaa54a3499d9422f5
Marc Gilleron <zylann_sagas_mp3@hotmail.fr>
2013-01-04 17:26:17 +0100

    Skeleton (again) for adding characters to rooms and rendering stuff

--- src/isr/Room.java
+++ src/isr/Room.java
@@ -1,11 +1,15 @@
 package isr;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class Room
 {
+	// Kinds of room
 	public static final int CORRIDOR = 0;
 	public static final int ENGINE = 1;
 	public static final int JAIL = 2;
@@ -13,11 +17,62 @@ public class Room
 	public static final int SLEEP = 4;
 	public static final int MAIN = 5;
 	
-	public void render(GameContainer gc, StateBasedGame game, Graphics gfx)
+	// Drawing layers
+	public static final int LAYER_FLOOR = 0;
+	public static final int LAYER_OBJECTS = 1;
+	public static final int LAYER_CHARACTERS = 2;
+	public static final int LAYER_CEILING = 3;
+	
+	/** Characters in the room **/
+	private HashSet<Character> characterRefs = new HashSet<Character>();
+	
+	/** Type of the room (corridor, engine, jail...) **/
+	private int type;
+	
+	public Room(int roomType)
 	{
-		// TODO Auto-generated method stub
-		
+		this.type = roomType;
+	}
+	
+	public void init()
+	{
+		// TODO Load the room layout from its type
+	}
+	
+	public void render(GameContainer gc, StateBasedGame game, Graphics gfx, int layer)
+	{
+		// TODO draw the room depending which layer is specified
 	}
 
+	public int getType()
+	{
+		return type;
+	}
 	
+	public void addCharacter(Character c)
+	{
+		if(isCharacterIn(c))
+		{
+			System.out.println(
+				"ERROR: cannot add a character to a room where it is already present");
+			return;
+		}
+		characterRefs.add(c);
+	}
+	
+	public void removeCharacter(Character c)
+	{
+		characterRefs.remove(c);
+	}
+	
+	public boolean isCharacterIn(Character c)
+	{
+		return characterRefs.contains(c);
+	}
+
 }
+
+
+
+
+
--- src/isr/Ship.java
+++ src/isr/Ship.java
@@ -14,8 +14,9 @@ public class Ship
 	private static final int ROOM_COUNT = 6;
 
 	private static Ship instance;
-	
+
 	private Room rooms[] = new Room[ROOM_COUNT];
+	private Character characters[];
 	
 	public static Ship get()
 	{
@@ -26,18 +27,26 @@ public class Ship
 	
 	public Ship()
 	{
-		rooms[Room.CORRIDOR] = new Room();
-		rooms[Room.ENGINE] = new Room();
-		rooms[Room.JAIL] = new Room();
-		rooms[Room.SPY] = new Room();
-		rooms[Room.SLEEP] = new Room();
-		rooms[Room.JAIL] = new Room();
+		// Rooms creations
+		setRoom(new Room(Room.CORRIDOR));
+		setRoom(new Room(Room.ENGINE));
+		setRoom(new Room(Room.JAIL));
+		setRoom(new Room(Room.SPY));
+		setRoom(new Room(Room.SLEEP));
+		setRoom(new Room(Room.MAIN));
+		
+		// Characters creation
+		characters = Character.createCrew(); // TODO uncomment when available
+	}
+	
+	private void setRoom(Room room)
+	{
+		rooms[room.getType()] = room;
 	}
 
 	public void render(GameContainer gc, StateBasedGame game, Graphics gfx)
 	{
-		for(Room r : rooms)
-			r.render(gc, game, gfx);
+		
 	}
 
 }
