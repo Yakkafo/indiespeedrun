b0f2daf84215c7ff3a843e1eeeefb3e792c384eb
unknown <emmanuel.corno@neuf.fr>
2013-01-06 08:29:22 +0100

    new report

--- src/isr/Ship.java
+++ src/isr/Ship.java
@@ -218,6 +218,7 @@ public class Ship
 			{
 				// Personne pour surveiller l'espion : l'ennemi avance plus vite
 				EnemyShip.get().advance(Spy.BAD_ACTION);
+				report.setSpyBadAction(true);
 				System.out.println("Manigances de l'espion");
 			}
 		} 
