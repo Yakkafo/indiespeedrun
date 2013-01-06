e75a76cd4bd8d72858b0b7f1e88fea57347c079d
unknown <emmanuel.corno@neuf.fr>
2013-01-06 08:44:01 +0100

    report

--- src/isr/Ship.java
+++ src/isr/Ship.java
@@ -212,7 +212,7 @@ public class Ship
 
 		// SPY
 		Room holdRoom = rooms[RoomType.HOLD.ordinal()];
-		if(holdRoom.getCharacterCount() == 0 || holdRoom.isTraitorInside())
+		if(holdRoom.getCharacterCount() == 0)
 		{
 			if(spy.isDoingBadAction()) // Chance pour que l'espion fasse son enfoiré
 			{
@@ -221,7 +221,17 @@ public class Ship
 				report.setSpyBadAction(true);
 				System.out.println("Manigances de l'espion");
 			}
-		} 
+		}
+		else if(holdRoom.isTraitorInside())
+		{
+			if(spy.isDoingBadAction()) // Chance pour que l'espion fasse son enfoiré
+			{
+				// Personne pour surveiller l'espion : l'ennemi avance plus vite
+				EnemyShip.get().advance(Spy.BAD_ACTION);
+				report.setSpyBadAction2(true);
+				System.out.println("Manigances de l'espion");
+			}
+		}
 
 	}
 
